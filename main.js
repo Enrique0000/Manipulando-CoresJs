34e96a13e8b01f1b63ac4c9b7027da5f30a2159d
Enrique Marques <89088074+Enrique0000@users.noreply.github.com>
2021-08-31 14:07:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var myp = document.querySelectorAll("p.pa");
+
+function aperte1(){
+   myp[0].style.backgroundColor = "blue";
+}
+function aperte2(){
+   myp[1].style.backgroundColor = "red";
+}
+function aperte3(){
+   myp[2].style.backgroundColor = "green";
+}
+function aperte4(){
+   myp[3].style.backgroundColor = "yellow";
+}
+function aperte5(){
+   myp[4].style.backgroundColor = "violet";
+}
